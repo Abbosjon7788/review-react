5f04e8cd6dffa995c3f2a242bf72c71bfa25332f
Abbosjon(U1910298) <nosirovabosjon3429@gmail.com>
2021-07-16 21:29:54 +0500

    something was changed

--- src/components/trello/Trello.js
+++ src/components/trello/Trello.js
@@ -3,8 +3,23 @@ import React, {Component} from 'react';
 class Trello extends Component {
     render() {
         return (
-            <div className="mx-20 my-10">
-                <h1>This is trello component!</h1>
+            <div className="flex mx-20 my-10">
+                <div className="w-1/5 px-3 pt-6">
+                    <div>
+                        <button type="button" className="rounded-md px-4 py-2 bg-green-400 hover:bg-green-500 text-white font-bold tracking-widest font-serif">Add board</button>
+                        <div className="p-4 mt-4 border rounded-md shadow-md">
+                            <textarea placeholder="typing..." className="border w-full rounded-md p-4 font-noto"/>
+                            <button type="button" className="px-4 py-2 mt-2 ml-auto block tracking-wider text-white bg-green-400 hover:bg-green-500 font-noto font-bold rounded-md">Add</button>
+                        </div>
+                    </div>
+                </div>
+
+                {/*this below code should be mapped*/}
+                <div className="w-1/5 px-3 pt-6">
+                    <div className="border p-4 rounded-md shadow-md">
+
+                    </div>
+                </div>
             </div>
         );
     }
